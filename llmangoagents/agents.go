eed0e0daf7e10c4a96b886a0875c92bdae5456a2
Carson <cdshortman@gmail.com>
2025-06-02 21:45:01 -0700

    working on agent now

--- llmangoagents/agents.go
+++ llmangoagents/agents.go
@@ -25,6 +25,7 @@ type Agent struct {
 	Name          string
 	SystemMessage string
 	Model         string
+	Parameters    string
 	Tools         []Tool
 }
 
@@ -44,6 +45,6 @@ type Workflow struct {
 }
 
 //Flow works like this. We have an entrypoint agent at each step this organizes the process or can be a single step agent for simple tasks basically decides whether to call tools or to return
-//if we have a exitpoint this acts as a validator agent which either accepts or denys the results 
+//if we have a exitpoint this acts as a validator agent which either accepts or denys the results
 //we use opneai spec for the tool calling if possible. or a generalizable format
 //for certain tools we can "initialize them " which puts a key or other thing into a map for them to access thus this can limit the destructive power of llms by forcing create or update to be limited to that key thus managing their control in sql environment
